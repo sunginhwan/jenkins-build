26b11844008dccf26a34db09c41d5e37539fc702
kosta <kosta@kosta-PC>
2016-07-01 10:20:37 +0900

    커밋테스트~

--- src/main/java/kr/co/kosta/hello_maven/App.java
+++ src/main/java/kr/co/kosta/hello_maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ!" );
+        System.out.println( "가나다라마바사!" );
     }
 }
