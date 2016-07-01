8674c8048e5cc5e9062aa7217c0c5bf1bbcdb9b5
kosta <kosta@kosta-PC>
2016-07-01 10:28:46 +0900

    333

--- src/main/java/kr/co/kosta/hello_maven/App.java
+++ src/main/java/kr/co/kosta/hello_maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "가나다라마바사아자차카타파하!" );
+        System.out.println( "가나다라마바사아자차카타파하asdsadsadsad!" );
     }
 }
